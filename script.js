b09b2abcb4a5163e17e78a9b484d87ed21b42607
tareu <cantilever10101010@gmail.com>
2025-06-09 08:36:26 +0530

    ..

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+
 window.onload = (event) => {
+        var div1= document.createElement("div");
+        div1.style.background = "powderblue";
+        div1.id = "div1"
+        var div2 = document.createElement("div");
+        div2.style.whiteSpace = "pre-line";
+        div2.id = "div2"
+        var div3 = document.createElement("div");
+        div3.innerHTML = "<br>";
+        div3.id = "div3"
+        document.getElementById("main").appendChild(div1);
+        document.getElementById("main").appendChild(div3);
+        document.getElementById("main").appendChild(div2);
+
 	fetch('https://raw.githubusercontent.com/tareu/texts/refs/heads/main/texts.txt')
 		.then(response => response.text())
 		.then((data) => {
 			document.getElementById("div2").innerHTML = data;
 		})
+
 	setTime();
 };
 
 function setTime() {
 	const d = new Date();
-	document.getElementById("div1").innerHTML = d;
+	document.getElementById("div1").innerHTML = d.toString();
 	setTimeout(setTime, 1000);
 }
 
